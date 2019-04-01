2bed048c6df804989abb8260207bc5ef7bc7290c
SunPengChuan <sunpengchuan@gmail.com>
2019-04-02 00:49:09 +0800

    nothing

---
+++ build/lib/wgdi/base.py
@@ -0,0 +1,165 @@
+import configparser
+import os
+import re
+
+import pandas as pd
+import numpy as np
+import wgdi
+from Bio import Seq, SeqIO, SeqRecord
+
+
+def config():
+    conf = configparser.ConfigParser()
+    conf.read(os.path.join(wgdi.__path__[0], 'conf.ini'))
+    return conf.items('ini')
+
+
+def load_conf(file, section):
+    conf = configparser.ConfigParser()
+    conf.read(file)
+    return conf.items(section)
+
+
+def read_colinearscan(file):
+    data, b, flag, num= [], [], 0, 1
+    with open(file) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if re.match(r"the", line):
+                num = re.search('\d+',line).group()
+                b = []
+                flag = 1
+                continue
+            if re.match(r"\>LOCALE", line):
+                flag = 0
+                p = re.split(':', line)
+                data.append([num,b,p[1]])
+                b = []
+                continue
+            if flag == 1:
+                a = re.split(r"\s", line)
+                b.append(a)
+    return data
+
+
+def read_mcscanx(fn):
+    f1 = open(fn)
+    data, b = [], []
+    flag = 0
+    for line in f1.readlines():
+        line = line.strip()
+        if re.match(r"## Alignment", line):
+            flag = 1
+            if len(b) == 0:
+                b.append([line])
+                continue
+            data.append(b)
+            b = []
+            b.append([line])
+            continue
+        if flag == 0:
+            continue
+        if re.match(r'#', line):
+            continue
+        a = re.split(r"\:", line)
+        c = re.split(r"\s+", a[1])
+        b.append([c[1], c[2]])
+    data.append(b)
+    return data
+
+
+def read_ks(file):
+    ks = pd.read_csv(file, sep='\t', header=None)
+    ks = ks.drop_duplicates()
+    ks = ks[ks[3] > 0]
+    ks.index = ks[0]+','+ks[1]
+    return ks
+
+
+def get_median(data):
+    if len(data) == 0:
+        return 0
+    data.sort()
+    half = len(data) // 2
+    return (data[half] + data[~half]) / 2
+
+
+def cds_to_pep(cds_file, pep_file, fmt='fasta'):
+    records = list(SeqIO.parse(cds_file, fmt))
+    for k in records:
+        k.seq = k.seq.translate()
+    SeqIO.write(records, pep_file, 'fasta')
+    return True
+
+
+def tendem(chr1, chr2, loc1, loc2):
+    if (chr1 == chr2) and (abs(float(loc1)-float(loc2)) < 200):
+        return True
+    return False
+
+
+def newblast(file, score, evalue, gene_loc1, gene_loc2):
+    blast = pd.read_csv(file, sep="\t", header=None)
+    blast = blast[(blast[11] >= score) & (
+        blast[10] < evalue) & (blast[1] != blast[0])]
+    blast = blast[(blast[0].isin(gene_loc1.index)) & (blast[1].isin(gene_loc2.index))]
+    blast.drop_duplicates(subset=[0, 1], keep='first', inplace=True)
+    blast[0] = blast[0].astype(str)
+    blast[1] = blast[1].astype(str)
+    return blast
+
+
+def newgff(file):
+    gff = pd.read_csv(file, sep="\t", header=None,index_col=1)
+    gff.rename(columns={0: 'chr', 2: 'start',
+                        3: 'end', 4: 'stand', 5: 'order'}, inplace=True)
+    gff['chr'] = gff['chr'].astype(str)
+    gff['start'] = gff['start'].astype(float)
+    gff['end'] = gff['end'].astype(float)
+    gff['stand'] = gff['stand'].astype(str)
+    gff['order'] = gff['order'].astype(int)
+    return gff
+
+
+def newlens(file, position):
+    lens = pd.read_csv(file, sep="\t", header=None, index_col=0)
+    lens.index = lens.index.astype(str)
+    if position == 'order':
+        lens = lens[2]
+    if position == 'end':
+        lens = lens[1]
+    return lens
+
+
+def gene_location(gff, lens, step, position):
+    loc_gene, dict_chr, n = {}, {}, 0
+    gff = gff[gff['chr'].isin(lens.index)]
+    dict_chr=dict(zip(lens.index, np.append(np.array([0]),lens.cumsum()[:-1].values)))
+    gff.loc[:,'loc']=''
+    for name, group in gff.groupby(['chr']):
+        gff.loc[group.index,'loc'] = (dict_chr[name]+group[position])*step
+    return gff
+
+
+def dotplot_frame(fig, ax, lens1, lens2, step1, step2, genome1_name, genome2_name):
+    for k in lens1.cumsum()[:-1]*step1:
+        ax.axhline(y=k, alpha=1, color='black', lw=0.5)
+    for k in lens2.cumsum()[:-1]*step2:
+        ax.axvline(x=k, alpha=1, color='black', lw=0.5)
+    align = dict(family='Times New Roman', style='normal',
+                 horizontalalignment="center", verticalalignment="center")
+    yticks = lens1.cumsum()*step1-0.5*lens1*step1
+    ax.set_yticks(yticks)
+    ax.set_yticklabels(lens1.index, fontsize=12, **align)
+    xticks = lens2.cumsum()*step2-0.5*lens2*step2
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(lens2.index, fontsize=12, **align)
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+    ax.axis([0, 1, 1, 0])
+    ax.set_ylabel(genome1_name, labelpad=8,
+                  weight='semibold', fontsize=18, **align)
+    fig.suptitle(genome2_name, weight='semibold', fontsize=18, **align)
+
+# if __name__ == "__main__":
+#     config()
---
+++ build/lib/wgdi/block_correspondence.py
@@ -0,0 +1,129 @@
+import re
+import sys
+
+import numpy as np
+import pandas as pd
+import wgdi.base as base
+
+
+class block_correspondence():
+    def __init__(self, options):
+        self.block_len = 0
+        self.correspondence = 'all'
+        self.tandem = True
+        for k, v in options:
+            setattr(self, str(k), v)
+            print(k, ' = ', v)
+        self.homo = [float(k) for k in self.homo.split(',')]
+
+    def run(self):
+        colinearity = base.read_colinearscan(self.colinearity)
+        lens1 = base.newlens(self.lens1, self.position)
+        lens2 = base.newlens(self.lens2, self.position)
+        gff1 = base.newgff(self.gff1)
+        gff2 = base.newgff(self.gff2)
+        homopairs = self.deal_blast(gff1, gff2)
+        if self.correspondence == 'all':
+            cor = [[k, i, 0, lens_1.at[i, 2], j, 0, lens_2.at[j, 2],float(self.homo[0]),float(self.homo[1])]
+                   for k in range(1, int(self.wgd)+1) for i in lens_1.index for j in lens_2.index]
+            cor = pd.DataFrame(
+                cor, columns=['sub', 'chr1', 'start1', 'end1', 'chr2', 'start2', 'end2','homo1','homo2'])
+            cor.to_csv('all.coor.txt', header=None, index=False)
+        else:
+            cor = pd.read_csv(self.correspondence, sep=',',
+                              header=None, engine='python')
+            cor.columns = ['sub', 'chr1', 'start1',
+                           'end1', 'chr2', 'start2', 'end2','homo1','homo2']
+        cor['chr1'] = cor['chr1'].astype(str)
+        cor['chr2'] = cor['chr2'].astype(str)
+        gff1 = pd.concat([gff1, pd.DataFrame(columns=list('L'+str(i)
+                                                          for i in range(1, int(self.wgd)+1)))], sort=True)
+        if self.tandem == True or self.tandem == 'true' or self.tandem == 1:
+            colinearity = self.remove_tandem(colinearity, gff1, gff2)
+        align = self.colinearity_region(
+            gff1, gff2, colinearity, cor, homopairs)
+        align[gff1.columns[-int(self.wgd):]
+              ].to_csv(self.savefile, sep='\t', header=None)
+
+    def deal_blast(self, gff1, gff2):
+        blast = base.newblast(self.blast, int(self.score), float(self.evalue), gene_loc1, gene_loc2)
+        blast = pd.read_csv(self.blast, sep="\t", header=None)
+        score, evalue, repnum = 200, 1e-5, 20
+        blast = blast[(blast[11] >= score) & (
+            blast[10] < evalue) & (blast[1] != blast[0])]
+        blast = blast[(blast[0].isin(gff1.index.values)) &
+                      (blast[1].isin(gff2.index.values))]
+        blast.drop_duplicates(subset=[0, 1], keep='first', inplace=True)
+        homopairs = {}
+        dupnum = int(self.wgd)
+        hitnum = dupnum+4
+        for name, group in blast.groupby([0])[1]:
+            newgroup = group.values.tolist()[:repnum]
+            for i, el in enumerate(newgroup, start=1):
+                if i <= dupnum:
+                    homopairs[name+","+el] = 1
+                elif i <= hitnum:
+                    homopairs[name+","+el] = 0
+                else:
+                    homopairs[name+","+el] = -1
+        return homopairs
+
+    def remove_tandem(self, colinearity, gff1, gff2):
+        newcolinearity = []
+        for k in colinearity:
+            block = []
+            chr1, chr2 = gff1.loc[k[0][0][0], 0], gff2.loc[k[0][0][2], 0]
+            if chr1 != chr2:
+                newcolinearity.append(k)
+                continue
+            for v in k[0]:
+                if base.tendem(chr1, chr2, v[1], v[3]):
+                    continue
+                block.append(v)
+            if len(block) == 0:
+                continue
+            k[0] = block
+            newcolinearity.append(k)
+        return newcolinearity
+
+    def colinearity_region(self, gff1, gff2, colinearity, cor, homopairs):
+        for k in colinearity:
+            if len(k[0]) <= int(self.block_len):
+                continue
+            chr1, chr2 = gff1.loc[k[0][0][0], 0], gff2.loc[k[0][0][2], 0]
+            array1, array2 = [float(i[1]) for i in k[0]], [
+                float(i[3]) for i in k[0]]
+            start1, end1 = min(array1), max(array1)
+            start2, end2 = min(array2), max(array2)
+            if (end1-start1)/len(array1)<=0.05 or (end2-start2)/len(array2)<=0.05:
+                continue
+            newcor = cor[(cor['chr1'] == str(chr1)) &
+                         (cor['chr2'] == str(chr2))]
+            group = newcor.drop_duplicates(
+                subset=['start1', 'end1', 'start2', 'end2'], keep='first', inplace=False)
+            for index, row in group.iterrows():
+                if (int(row['start1']) <= start1) and (int(row['end1']) >= end1) and (int(row['start2']) <= start2) and (int(row['end2']) >= end2):
+                    homo = 0
+                    for block in k[0]:
+                        if (block[0] not in gff1.index) or (block[2] not in gff2.index):
+                            continue
+                        if block[0]+","+block[2] in homopairs.keys():
+                            homo += homopairs[block[0]+","+block[2]]
+                    homo = homo/len(k[0])
+                    if homo <= float(row['homo1']) or homo >= float(row['homo2']):
+                        continue
+                    index = gff1[(gff1[0] == chr1) & (gff1[5] >= start1) & (
+                        gff1[5] <= end1)].index
+                    new_index = [i[0] for i in k[0]]
+                    for i in range(1, int(self.wgd)+1):
+                        name = 'L'+str(i)
+                        old_index = gff1[gff1.index.isin(
+                            index) & gff1[name].str.match(r'\w+') == True].index
+                        inters = np.intersect1d(old_index, new_index)
+                        if len(inters)/len(new_index) > 0.2:
+                            continue
+                        gff1.loc[gff1.index.isin(
+                            index) & gff1[name].isnull(), name] = '.'
+                        gff1.loc[new_index, name] = [i[2] for i in k[0]]
+                        break
+        return gff1
